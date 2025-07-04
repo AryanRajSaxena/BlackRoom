ddacb15a34fc134d393d4301a96d7c81da587dcb
Aryan Raj Saxena <aryanrajsaxena@Aryans-MacBook-Air.local>
2025-07-04 12:08:00 +0530

    build successfully

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ✅ important for Netlify
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
